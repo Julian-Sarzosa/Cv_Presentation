f6a74450ab8762c68d5cb348e1d995c2dd83f9d7
Julian-Sarzosa <ojsarzosa@misena.edu.co>
2021-01-19 14:40:48 -0500

    Create Proyect in GitHub

---
+++ app/src/main/java/com/julian_sarzosa/cv_presentation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.julian_sarzosa.cv_presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
